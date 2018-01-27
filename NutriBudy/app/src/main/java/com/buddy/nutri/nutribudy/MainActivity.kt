79e87087d20bc898cfedf2bfa8c74d67b6577754
Fozail Ahmad <fozail_113@sympatico.ca>
2018-01-27 16:25:45 -0500

    Splash Page

--- NutriBudy/app/src/main/java/com/buddy/nutri/nutribudy/MainActivity.kt
+++ NutriBudy/app/src/main/java/com/buddy/nutri/nutribudy/MainActivity.kt
@@ -3,9 +3,11 @@ package com.buddy.nutri.nutribudy
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
