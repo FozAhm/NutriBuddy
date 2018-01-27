049a98a23a4a1359284c509acdf6f360be979fdd
Fozail Ahmad <fozail_113@sympatico.ca>
2018-01-27 17:16:31 -0500

    Added Image Connections to Class

--- NutriBudy/app/src/main/java/com/buddy/nutri/nutribudy/MainActivity.kt
+++ NutriBudy/app/src/main/java/com/buddy/nutri/nutribudy/MainActivity.kt
@@ -2,13 +2,24 @@ package com.buddy.nutri.nutribudy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.view.View
 
 class MainActivity : AppCompatActivity()
 {
-
+    private var splashImage: ImageButton? = null
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setuoUI()
+
+    }
+
+    fun setuoUI()
+    {
+        splashImage = findViewById<ImageButton>(R.id.splashButton) as ImageButton
     }
 }
