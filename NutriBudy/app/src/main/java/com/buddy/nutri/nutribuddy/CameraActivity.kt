bcfa2d0588a35ccc9a32948458c4a260d8840713
Colin Barber <colinbmusic@gmail.com>
2018-01-28 09:23:44 -0500

    :)

--- NutriBudy/app/src/main/java/com/buddy/nutri/nutribuddy/CameraActivity.kt
+++ NutriBudy/app/src/main/java/com/buddy/nutri/nutribuddy/CameraActivity.kt
@@ -23,6 +23,7 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_camera)
         savedInstanceState ?: supportFragmentManager.beginTransaction()
                 .replace(R.id.container, Camera2BasicFragment.newInstance())
